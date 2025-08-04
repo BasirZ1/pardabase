aaa8c52851cdd44b0eeed3f1f5222faf38dd0e98
BasirZ1 <abdulbasirzurmati9@gmail.com>
2025-08-04 15:46:50 +0430

    notify.py edited
    load_env() added to notify.py

--- run.py
+++ run.py
@@ -2,14 +2,14 @@ import asyncio
 
 from db import get_drafts_list_ps, get_cutting_history_list_ps, insert_new_purchase, insert_new_supplier, \
     update_supplier, get_supplier_ps, get_suppliers_list_ps, remove_supplier_ps
-from helpers.general import get_date_range_test
+from telegram import send_notification
 from utils import set_current_db
+from utils.config import ADMIN_TELEGRAM
 
 
 async def run_this():
     # set_current_db("zmt")
-    date_range = get_date_range_test(0)
-    print(date_range)
+    await send_notification(ADMIN_TELEGRAM, "testing")
     # await get_recent_activities_preview()
     # # await update_user("basirzurmati", "Basir Zurmati",
     # #                   "basirzurmati", 5, "@Basir7233$")
--- telegram/notify.py
+++ telegram/notify.py
@@ -1,12 +1,13 @@
 import os
 
 from aiogram import Bot
+from dotenv import load_dotenv
 
+load_dotenv(override=True)
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-PRIVATE_KEY_PATH = "private.key"
 
 
-async def send_notification(chat_id: int, text: str):
+async def send_notification(chat_id: str, text: str):
     bot = Bot(token=TOKEN)
     await bot.send_message(chat_id=chat_id, text=text)
     await bot.session.close()
