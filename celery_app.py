1a2a257fd4ebbcbfbb06eecc6c94de1855eb3dbc
BasirZ1 <abdulbasirzurmati9@gmail.com>
2025-08-18 01:43:15 +0430

    minor bugs

--- celery_app.py
+++ celery_app.py
@@ -1,11 +1,17 @@
+import os
+
 from celery import Celery
 from celery.schedules import crontab
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+redis_url = os.getenv("RED_URL")
 
 celery_app = Celery(
     "curtains_tasks",
-    broker="redis://localhost:6379/1"
+    broker=f"{redis_url}/1"
 )
-celery_app.conf.timezone = "Kabul/Asia"
+celery_app.conf.timezone = "Asia/Kabul"
 
 
 # Periodic scheduled task
