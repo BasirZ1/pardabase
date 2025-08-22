68dd13a0810303c480646862564c91ca675de823
BasirZ1 <abdulbasirzurmati9@gmail.com>
2025-08-22 15:48:58 +0430

    debugging celery

--- tasks/user.py
+++ tasks/user.py
@@ -4,7 +4,7 @@ from celery_app import celery_app
 from db import get_emails_high_clearance_users_ps
 from db.earning import calculate_all_due_salaries_with_report_ps
 from helpers import send_salary_report_email
-from utils import set_current_db
+from utils import set_current_db, flatbed
 
 
 @celery_app.task
@@ -40,6 +40,7 @@ def scheduled_salary_calculations_with_email():
             raise Exception("No results returned from salary calculation function")
 
     except Exception as e:
+        asyncio.run(flatbed("exception", f"In celery salary_calculations_with_email: {e}"))
         return {
             "status": "error",
             "error": str(e),
