e684bc24e3539f3350356923dacdf9699d2836c5
BasirZ1 <abdulbasirzurmati9@gmail.com>
2025-09-03 00:27:35 +0430

    added sync to notifications

--- db/notification.py
+++ db/notification.py
@@ -1,28 +1,28 @@
+from typing import Optional
+
+from helpers import parse_date
 from utils import flatbed
 from utils.conn import connection_context
 
 
-async def get_notifications_for_user_ps(user_id: str, level: int):
-    """
-    Retrieve all notifications for a specific user from notifications table.
+async def get_notifications_for_user_ps(user_id: str, level: int, old_sync: Optional[str] = None):
+    if old_sync:
+        old_sync = parse_date(old_sync)
+    else:
+        old_sync = '1970-01-01T00:00:00Z'  # fetch all if not provided
 
-    Returns:
-    - All users from the notifications table.
-    """
     try:
         async with connection_context() as conn:
             query = """
                 select *
                 from notifications n
-                join users u on u.user_id = $1  -- current user
-                where
-                    n.target_user_id = u.user_id  -- personal notifications
-                    or ($2 = ANY(n.target_roles)) -- role-based
+                where (n.target_user_id = $1 or $2 = ANY(n.target_roles))
+                  and n.created_at > $3
                 order by n.created_at desc
                 limit 50;
             """
-            notifications_list = await conn.fetch(query, user_id, level)
-            return notifications_list  # Returns a list of asyncpg Record objects
+            notifications_list = await conn.fetch(query, user_id, level, old_sync)
+            return notifications_list  # list of asyncpg.Record
 
     except Exception as e:
         await flatbed('exception', f"In get_notifications_for_user_ps: {e}")
--- routes/notification.py
+++ routes/notification.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from dotenv import load_dotenv
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
@@ -12,11 +14,12 @@ load_dotenv(override=True)
 
 @router.get("/notifications-for-user-get")
 async def get_notifications_for_user(
+        oldSync: Optional[str] = None,
         user_data: dict = Depends(verify_jwt_user(required_level=1))
 ):
     """
     Retrieve a list of notifications for user.
     """
-    notifications_data = await get_notifications_for_user_ps(user_data['user_id'], user_data['level'])
+    notifications_data = await get_notifications_for_user_ps(user_data['user_id'], user_data['level'], oldSync)
     notifications_list = get_formatted_notifications_list(notifications_data)
     return JSONResponse(content=notifications_list, status_code=200)
