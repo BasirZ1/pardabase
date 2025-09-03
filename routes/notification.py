99565260a2cd1b2ce569ac4e4150e0ccde0925b3
BasirZ1 <abdulbasirzurmati9@gmail.com>
2025-09-03 12:59:14 +0430

    debugging

--- routes/notification.py
+++ routes/notification.py
@@ -6,7 +6,7 @@ from fastapi.responses import JSONResponse
 
 from db import get_notifications_for_user_ps
 from helpers import get_formatted_notifications_list
-from utils import verify_jwt_user
+from utils import verify_jwt_user, flatbed
 
 router = APIRouter()
 load_dotenv(override=True)
@@ -20,6 +20,8 @@ async def get_notifications_for_user(
     """
     Retrieve a list of notifications for user.
     """
+    await flatbed("debug", f"here is oldSync {oldSync}, here is user_id {user_data['user_id']}, here is user_level {user_data['level']}")
     notifications_data = await get_notifications_for_user_ps(user_data['user_id'], user_data['level'], oldSync)
+    await flatbed("debug", f"here are the results {str(notifications_data)}")
     notifications_list = get_formatted_notifications_list(notifications_data)
     return JSONResponse(content=notifications_list, status_code=200)
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from utils import set_current_db
 
 async def run_this():
     set_current_db("zmt")
-    data = await get_notifications_for_user_ps('0bc3e678-00d4-4ed2-8901-dbaa79a86c4e', 5, None)
+    data = await get_notifications_for_user_ps('0bc3e678-00d4-4ed2-8901-dbaa79a86c4e', 1, None)
     print(data)
 
 asyncio.run(run_this())
